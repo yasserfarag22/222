3aec689a881f3e63fdf480fa65bae83f890a7b0a
yasser <yasserfarag575@gmail.com>
2023-05-10 20:17:27 +0300

    first commit

---
+++ App/Config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+// define pase url for the project 
+define("BURL", "http://127.0.0.1/oop/crud_mvc/public/");
+
+
+/**
+ * database configuration 
+ */
+
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "crud_mvc");
